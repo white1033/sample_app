079db6cbe85d192a1aacfffc4edcd78fcf6348bd
white1033 <k75715@gmail.com>
2014-05-04 14:12:34 +0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
   has_secure_password
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
 end
