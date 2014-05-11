c8d8262e47899aafcbbfe0cdea2acf29baa2ccad
white1033 <k75715@gmail.com>
2014-05-11 10:25:04 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -40,7 +41,7 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = "user deleted"
     redirect_to users_url
   end
 
@@ -51,14 +52,7 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
-  # Before actions
-
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: 'Please sign in'
-    end
-  end
+  # before actions
 
   def correct_user
     @user = User.find(params[:id])
